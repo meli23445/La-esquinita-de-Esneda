531a78791cd66e531527ae24d92d41e0d3c3ba25
Danna <dmq.123.n@gmail.com>
2026-02-22 18:08:34 -0500

    cambios 1

--- mail/contact.js
+++ mail/contact.js
@@ -1,4 +1,6 @@
 $(function () {
+    var WHATSAPP_NUMBER = '573182105940';
+    var WHATSAPP_BASE = 'https://wa.me/' + WHATSAPP_NUMBER;
 
     $("#contactForm input, #contactForm textarea").jqBootstrapValidation({
         preventSubmit: true,
@@ -7,47 +9,23 @@ $(function () {
         submitSuccess: function ($form, event) {
             event.preventDefault();
             var name = $("input#name").val();
-            var email = $("input#email").val();
+            var phone = $("input#phone").val();
             var subject = $("input#subject").val();
             var message = $("textarea#message").val();
 
-            $this = $("#sendMessageButton");
-            $this.prop("disabled", true);
+            var msg = '*Contacto - La Esquinita de Esneda*\n\n';
+            msg += 'Nombre: ' + name + '\n';
+            msg += 'Teléfono: ' + phone + '\n';
+            msg += 'Asunto: ' + subject + '\n\n';
+            msg += 'Mensaje: ' + message;
+            var url = WHATSAPP_BASE + '?text=' + encodeURIComponent(msg);
+            window.open(url, '_blank', 'noopener,noreferrer');
 
-            $.ajax({
-                url: "contact.php",
-                type: "POST",
-                data: {
-                    name: name,
-                    email: email,
-                    subject: subject,
-                    message: message
-                },
-                cache: false,
-                success: function () {
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                            .append("</button>");
-                    $('#success > .alert-success')
-                            .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                            .append('</div>');
-                    $('#contactForm').trigger("reset");
-                },
-                error: function () {
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                            .append("</button>");
-                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that our mail server is not responding. Please try again later!"));
-                    $('#success > .alert-danger').append('</div>');
-                    $('#contactForm').trigger("reset");
-                },
-                complete: function () {
-                    setTimeout(function () {
-                        $this.prop("disabled", false);
-                    }, 1000);
-                }
-            });
+            $('#success').html("<div class='alert alert-success'>");
+            $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>");
+            $('#success > .alert-success').append("<strong>Se abrirá WhatsApp para enviar tu mensaje.</strong>");
+            $('#success > .alert-success').append('</div>');
+            $('#contactForm').trigger("reset");
         },
         filter: function () {
             return $(this).is(":visible");
